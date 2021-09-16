300d9a1a5de0cd14824c44c3766a4609bc5c04cf
0MohamedHisham0 <MohamedDal4300@gmail.com>
2021-09-16 18:06:47 +0200

    Home Screen Done.

--- app/src/main/java/com/osama/answerwin/Utils/Constants.java
+++ app/src/main/java/com/osama/answerwin/Utils/Constants.java
@@ -3,9 +3,11 @@ package com.osama.answerwin.Utils;
 import com.google.firebase.firestore.FirebaseFirestore;
 
 public class Constants {
+
     private static FirebaseFirestore db;
 
     public static FirebaseFirestore GetFireStoneDb() {
+
         if (db == null)
             db = FirebaseFirestore.getInstance();
 
