0a5c11975c62a94afd1dea243935a776a9c30b33
Asem Ammar <asemammar99@gmail.com>
2021-10-04 22:36:49 +0200

    Final Edits.

--- app/src/main/java/com/osama/answerwin/Activities/SplashScreen.java
+++ app/src/main/java/com/osama/answerwin/Activities/SplashScreen.java
@@ -32,6 +32,7 @@ public class SplashScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+
         if (Constants.checkInternetConnection(this)) {
 
             new Handler().postDelayed(new Runnable() {
