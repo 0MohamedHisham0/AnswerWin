1b3f1bed9eaec0c5797e38b860e36d6564f1cd10
Asem Ammar <asemammar99@gmail.com>
2021-10-04 22:55:05 +0200

    Final Edits.

--- app/src/main/java/com/osama/answerwin/Utils/Constants.java
+++ app/src/main/java/com/osama/answerwin/Utils/Constants.java
@@ -3,7 +3,9 @@ package com.osama.answerwin.Utils;
 import android.app.Dialog;
 import android.content.Context;
 import android.content.Intent;
+import android.content.res.AssetManager;
 import android.graphics.drawable.ColorDrawable;
+import android.inputmethodservice.Keyboard;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.text.format.DateFormat;
@@ -27,13 +29,25 @@ import com.osama.answerwin.Models.BooledModel;
 import com.osama.answerwin.Models.UserModel;
 import com.osama.answerwin.R;
 
+import org.apache.poi.hssf.usermodel.HSSFCell;
+import org.apache.poi.hssf.usermodel.HSSFRow;
+import org.apache.poi.hssf.usermodel.HSSFSheet;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.poifs.filesystem.POIFSFileSystem;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+
+import java.io.InputStream;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Locale;
 import java.util.Objects;
+
+
 
 public class Constants {
 
@@ -196,6 +210,54 @@ public class Constants {
 
 
     }
+
+    public void readExcelFileFromAssets(Context context) {
+        try {
+            InputStream myInput;
+            // initialize asset manager
+            AssetManager assetManager = context.getAssets();
+            //  open excel sheet
+            myInput = assetManager.open("myexcelsheet.xls");
+
+            // Create a POI File System object
+
+            POIFSFileSystem myFileSystem = new POIFSFileSystem(myInput);
+
+            // Create a workbook using the File System
+            HSSFWorkbook myWorkBook = new HSSFWorkbook(myFileSystem);
+
+            // Get the first sheet from workbook
+            HSSFSheet mySheet = myWorkBook.getSheetAt(0);
+
+            // We now need something to iterate through the cells.
+            Iterator<Row> rowIter = mySheet.rowIterator();
+
+            int rowno =0;
+            while (rowIter.hasNext()) {
+                HSSFRow myRow = (HSSFRow) rowIter.next();
+                if(rowno !=0) {
+                    Iterator<Cell> cellIter = myRow.cellIterator();
+                    int colno =0;
+                    String sno="", date="", det="";
+                    while (cellIter.hasNext()) {
+                        HSSFCell myCell = (HSSFCell) cellIter.next();
+                        if (colno==0){
+                            sno = myCell.toString();
+                        }else if (colno==1){
+                            date = myCell.toString();
+                        }else if (colno==2){
+                            det = myCell.toString();
+                        }
+                        colno++;
+                    }
+                }
+                rowno++;
+            }
+        } catch (Exception e) {
+
+        }
+    }
+
 
 
 }
